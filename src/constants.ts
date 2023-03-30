ed70c239c7b1e1f1113bebc67c0eac39d3ff804d
Lizaveta Zinovich <zinovich_e@lwo.by>
2023-03-30 11:38:15 +0300

    fix: fix api links

--- src/constants.ts
+++ src/constants.ts
@@ -2,35 +2,24 @@ export const authLinks = {
   login: "/api/auth/login",
   registerClient: "/api/auth/registration",
   registerBrigadier: "/api/auth/brigadier/registration",
-};
-
-export const toolsLinks = {
-  getAll: "/api/tool",
-  get: (id: number) => `/api/tool/${id}`,
-  create: "/api/tool",
-  update: (id: number) => `/api/tool/${id}`,
-  delete: (id: number) => `/api/tool/${id}`,
+  activate: (link: string) => `api/auth/activate/${link}`,
+  requestResetPassword: "/api/auth/reset-password",
+  resetPassword: (login: string, link: string) => `api/auth/reset-password/${login}/${link}`,
+  userinfo: "/api/auth/userinfo",
 };
 
 export const accessoriesLinks = {
   getAll: "/api/accessory",
   get: (id: number) => `/api/accessory/${id}`,
   create: "/api/accessory",
+  import: "/api/accessory/import",
   update: (id: number) => `/api/accessory/${id}`,
   delete: (id: number) => `/api/accessory/${id}`,
 };
 
-export const equipmentLinks = {
-  getAll: "/api/equipment",
-  get: (id: number) => `/api/equipment/${id}`,
-  create: "/api/equipment",
-  update: (id: number) => `/api/equipment/${id}`,
-  delete: (id: number) => `/api/equipment/${id}`,
-};
-
 export const brigadierLinks = {
   getAll: "/api/brigadier",
-  getAlNotActivated: "/api/brigadier",
+  getAllNotActivated: "/api/brigadier",
   get: (id: number) => `/api/brigadier/${id}`,
   update: (id: number) => `/api/brigadier/${id}`,
   delete: (id: number) => `/api/brigadier/${id}`,
@@ -39,13 +28,16 @@ export const brigadierLinks = {
 export const clientLinks = {
   getAll: "/api/client",
   get: (id: number) => `/api/client/${id}`,
-  create: "/api/client",
   update: (id: number) => `/api/client/${id}`,
-  delete: (id: number) => `/api/client/${id}`,
 };
 
-export const stageLinks = {
-  getAll: "/api/stage",
+export const equipmentLinks = {
+  getAll: "/api/equipment",
+  get: (id: number) => `/api/equipment/${id}`,
+  create: "/api/equipment",
+  import: "/api/accessory/import",
+  update: (id: number) => `/api/equipment/${id}`,
+  delete: (id: number) => `/api/equipment/${id}`,
 };
 
 export const invoiceLinks = {
@@ -58,25 +50,47 @@ export const invoiceLinks = {
 export const requestLinks = {
   getAll: "/api/request",
   getWeeklyReport: "/api/request/weekly-report",
+  getWeeklyReportForBrigadier: (brigadierId: number) => `/api/request/weekly-report/${brigadierId}`,
   getAccessories: (id: number) => `/api/request/${id}/accessories`,
   getTools: (id: number) => `/api/request/${id}/tools`,
-  getEquipment: (id: number) => `/api/request/${id}/equipment`,
   getBrigadierRequests: (brigadierId: number) => `/api/request/brigadier/${brigadierId}`,
   getClientRequests: (clientId: number) => `/api/request/client/${clientId}`,
+  getFullReport: (id: number) => `/api/request/${id}/report`,
   get: (id: number) => `/api/request/${id}`,
   create: "/api/request",
-  update: (id: number) => `/api/request/${id}`,
-  updateByBrigadier: (id: number) => `/api/request/brigadier/update/${id}`,
-  updateByAdmin: (id: number) => `/api/request/admin/update/${id}`,
-  delete: (id: number) => `/api/request/${id}`,
+  updateByBrigadier: (id: number) => `/api/request/brigadier/${id}`,
+  updateByAdmin: (id: number) => `/api/request/admin/${id}`,
+};
+
+export const requestReportLinks = {
+  get: (requestId: number) => `/api/${requestId}/request-report`,
+  create: (requestId: number) => `/api/${requestId}/request-report`,
 };
 
 export const scheduleLinks = {
+  // TODO ???
   getAll: "/api/schedule",
   getByBrigadierId: (brigadierId: number) => `/api/schedule/${brigadierId}`,
 };
 
+export const stageLinks = {
+  getAll: "/api/stage",
+};
+
+export const toolsLinks = {
+  getAll: "/api/tool",
+  get: (id: number) => `/api/tool/${id}`,
+  create: "/api/tool",
+  import: "/api/accessory/import",
+  update: (id: number) => `/api/tool/${id}`,
+  delete: (id: number) => `/api/tool/${id}`,
+};
+
 export const userLinks = {
+  getAll: "/api/user",
+  get: (id: number) => `/api/user/${id}`,
+  update: (id: number) => `/api/user/${id}`,
+  delete: (id: number) => `/api/user/${id}`,
   activate: (id: number) => `/api/user/${id}/activate`,
   deactivate: (id: number) => `/api/user/${id}/deactivate`,
 };
