b6c2cc09d25abc5368b5104e84732e3db532963f
Lizaveta Zinovich <lizavetazinovich@gmail.com>
2023-04-10 16:56:28 +0300

    fix: add date with timestamp

--- src/components/admin/editRequestAdmin/BrigadierHistoryItem.tsx
+++ src/components/admin/editRequestAdmin/BrigadierHistoryItem.tsx
@@ -1,10 +1,14 @@
+import DayJs from "react-dayjs";
+
 function BrigadierHistoryItem({ item }: { item: unknown }) {
-  // const datetime = dayjs(item.modifiedDate).format("DD/MM/YYYY hh:mm");
+  // const datetime = dayjs(item.modifiedDate).format();
   return (
     <li className="flex flex-row h-full">
       <div className="flex justify-between p-2 gap-5 w-full">
         <div className="text-sm">{item.brigadier}</div>
-        <div className="text-gray-600 text-sm">{item.modifiedDate}</div>
+        <div className="text-gray-600 text-sm">
+          <DayJs format="DD.MM.YYYY в HH:mm">{item.modifiedDate}</DayJs>
+        </div>
       </div>
     </li>
   );
