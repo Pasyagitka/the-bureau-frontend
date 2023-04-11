bcab6385cd0dcefc17e8c71a61ec703ae865336d
Lizaveta Zinovich <zinovich_e@lwo.by>
2023-04-11 12:43:46 +0300

    refactor: null house

--- src/components/request/requestList/RequestSmall.tsx
+++ src/components/request/requestList/RequestSmall.tsx
@@ -26,8 +26,8 @@ function RequestSmall({ request, handleDownload }: { request: RequestDto; handle
         <Link to={`${request.id}`} className="p-2 w-2/3">
           <h1 className="text-gray-900 font-bold text-2xl">Заявка №{request.id}</h1>
           <p className="mt-2 text-gray-600 text-sm">
-            {`${request.address?.city}, ул.${request.address?.street}, дом ${request.address?.house} ${
-              request.address?.flat ?? ""
+            {`${request.address?.city}, ул.${request.address?.street}, дом ${request.address?.house}${
+              request.address?.flat ? `, кв ${request.address?.flat}` : ""
             }`}
           </p>
           {/* <div className="flex flex-wrap items-center gap-2 my-5">
