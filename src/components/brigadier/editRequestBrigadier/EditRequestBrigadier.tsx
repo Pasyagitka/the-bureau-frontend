abda360ea257d014d0091c67f9c8e1c2f182b0cc
Lizaveta Zinovich <lizavetazinovich@gmail.com>
2023-04-16 20:05:41 +0300

    fix: edit request by brigadier (status issues, request report files)

--- src/components/brigadier/editRequestBrigadier/EditRequestBrigadier.tsx
+++ src/components/brigadier/editRequestBrigadier/EditRequestBrigadier.tsx
@@ -1,12 +1,14 @@
+/* eslint-disable consistent-return */
 import SubmitButton from "@/elements/buttons/SubmitButton";
 import { useAppDispatch, useAppSelector } from "@/hooks";
 import { get, updateByBrigadier } from "@/redux/actions/requests";
-import { RequestStatus } from "@/types/enum/request-statuses.enum";
+import { RequestStatus, requestStatusesTitles } from "@/types/enum/request-statuses.enum";
 import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Uploader } from "rsuite";
 import CameraRetroIcon from "@rsuite/icons/legacy/CameraRetro";
 import { getAll as getAllRequestReports, patch } from "@/redux/actions/requestReports";
+import PhotoGallery from "@/elements/photoGallery/PhotoGallery";
 
 function EditRequestBrigadier() {
   const navigate = useNavigate();
@@ -19,7 +21,12 @@ function EditRequestBrigadier() {
   const [statusId, setStatus] = useState();
   const [files, setFiles] = useState([]);
 
-  const statuses = Object.values(RequestStatus).map((i) => <option selected={statusId === i} value={i} label={i} />);
+  const statuses = Object.values(RequestStatus).map((i) => {
+    if (i !== RequestStatus.APPROVED && i !== RequestStatus.INPROCESSING) {
+      console.log(i, requestStatusesTitles[i]);
+      return <option selected={statusId === i} value={i} label={requestStatusesTitles[i]} />;
+    }
+  });
 
   useEffect(() => {
     dispatch(get(params.id));
@@ -30,13 +37,32 @@ function EditRequestBrigadier() {
     setStatus(request?.status);
   }, [request]);
 
-  useEffect(() => {
-    setFiles(existingRequestReports.map((i) => ({ ...i })));
-  }, [existingRequestReports]);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     setFiles(existingRequestReports?.map((i) => getBlob(i)));
+  //     const res = await Promise.all(existingRequestReports?.map((i) => getBlob(i.url)));
+  //     console.log(res);
+  //   }
+  //   fetchData();
+  // }, [existingRequestReports]);
+
+  // useEffect(() => {
+  //   setFiles(existingRequestReports.map((i) => ({ ...i })));
+  // }, [existingRequestReports]);
+
+  const getBlob = async (url: string) => (await fetch(url)).blob();
 
   const handleSubmit = async () => {
-    const updateDto = { id: params.id, updateRequestByBrigadierDto: { status: statusId } };
-    const statusRes = await dispatch(updateByBrigadier(updateDto));
+    console.log(statusId);
+    if (statusId !== RequestStatus.APPROVED && statusId !== RequestStatus.INPROCESSING) {
+      const updateDto = {
+        id: params.id,
+        updateRequestByBrigadierDto: {
+          status: statusId,
+        },
+      };
+      const statusRes = await dispatch(updateByBrigadier(updateDto));
+    }
     console.log(files);
     const formData = new FormData();
     files.forEach((file, i) => {
@@ -64,21 +90,34 @@ function EditRequestBrigadier() {
           </div>
         </div>
         <div className="space-y-6 bg-white">
-          <div className="items-center w-full p-4 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
-            <div className="max-w-sm mx-auto space-y-5 md:w-2/3">
-              <select
-                name="status"
-                defaultValue={statusId}
-                className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-lime-500 focus:outline-none focus:ring-lime-500 sm:text-sm text-gray-700 placeholder-gray-400"
-                onChange={() => handleStatusSelectChange(e.currentTarget.value)}
-              >
-                {statuses}
-              </select>
+          {statusId === RequestStatus.APPROVED || statusId === RequestStatus.INPROCESSING ? (
+            <h1 className="text-gray-600 w-full text-center">Нельзя сменить статус заявки</h1>
+          ) : (
+            <div className="items-center w-full p-4 space-y-4 text-gray-500 md:inline-flex md:space-y-0">
+              <div className="max-w-sm mx-auto space-y-5 md:w-2/3">
+                <select
+                  name="status"
+                  defaultValue={statusId}
+                  className="mt-1 block w-full rounded-md border border-gray-300 bg-white py-2 px-3 shadow-sm focus:border-lime-500 focus:outline-none focus:ring-lime-500 sm:text-sm text-gray-700 placeholder-gray-400"
+                  onChange={handleStatusSelectChange}
+                >
+                  {statuses}
+                </select>
+              </div>
             </div>
+          )}
+          <hr />
+          <div className="items-center w-full p-4 space-y-4 text-gray-500  md:space-y-0">
+            <h1 className="text-gray-600 w-full text-center">Отчетность по заявке</h1>
+            {existingRequestReports && existingRequestReports.length > 0 ? (
+              <PhotoGallery images={existingRequestReports} />
+            ) : (
+              <dt className="text-sm font-medium text-gray-500">Отсутствует</dt>
+            )}
           </div>
           <hr />
           <div className="items-center w-full p-4 space-y-4 text-gray-500  md:space-y-0">
-            <h1 className="text-gray-600 w-full text-center">Добавить отчетность</h1>
+            <h1 className="text-gray-600 w-full text-center">Изменить отчетность</h1>
             <div className="flex">
               <div className="inline-flex items-center space-x-4">
                 <Uploader
