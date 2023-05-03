defe5d0fada952d50da42aeb31a63b32a6479ecc
Lizaveta Zinovich <zinovich_e@lwo.by>
2023-05-03 14:27:17 +0300

    refactor: make client page adaptive, fix mounting date field for leave req form

--- src/pages/client/ClientRequests.tsx
+++ src/pages/client/ClientRequests.tsx
@@ -5,10 +5,10 @@ function ClientRequests({ requests }: { requests: ClientRequestDto[] }) {
   const listItems = requests.map((item) => <ClientRequestItem key={item.id} request={item} />);
 
   return (
-    <div className="w-full bg-white p-12 rounded">
-      <div className="header flex items-end justify-between mb-12">
+    <div className="w-full bg-white p-2 md:p-12 rounded">
+      <div className="header flex items-end justify-between md:mb-12">
         <div className="title">
-          <p className="text-4xl font-bold text-gray-800 mb-4">Заявки</p>
+          <p className="md:text-4xl text-xl font-bold text-gray-800 mb-4">Заявки</p>
           {/* <p className="text-2xl font-light text-gray-400">Описание...</p> */}
         </div>
       </div>
